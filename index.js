b16f25a9f55fbb65c84a00e3592e1b3ea3535d4d
i5ting <i5ting@126.com>
2016-12-11 00:05:50 +0800

    lazycommmit, please ignore

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ const BaseController = require('slet-basecontroller')
 // https://github.com/koa-modules/multer
 const multer = require('koa-multer')
 
-module.exports = class BasicController extends BaseController{
+module.exports = class UploadController extends BaseController{
   constructor(app, ctx, next) {
     super(app, ctx, next)
-    
     
     if (!this.app.opts.upload) {
       console.log('please give a upload config')
