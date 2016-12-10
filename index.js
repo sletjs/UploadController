2165774f4a9f345f223eea3f20fbc174e9ef0bea
i5ting <i5ting@126.com>
2016-12-10 23:59:33 +0800

    lazycommmit, please ignore

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 'use strict';
 
 const BaseController = require('slet-basecontroller')
+// https://github.com/koa-modules/multer
+const multer = require('koa-multer')
 
 module.exports = class BasicController extends BaseController{
   constructor(app, ctx, next) {
     super(app, ctx, next)
     
-    this.query = ctx.query
-
-    this.global_filter = ['koa-bodyparser']
+    
+    if (!this.app.opts.upload) {
+      console.log('please give a upload config')
+      return;
+    }
+    
+    // 定义中间件
+    this.upload = multer(this.app.opts.upload) 
   }
 }
